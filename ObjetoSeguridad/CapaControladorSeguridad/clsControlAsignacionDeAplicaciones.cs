5142e43cc3c6df9ef7d3c9b878f39cfca526802c
jmoratayat <jmoratayat2@miumg.edu.gt>
2020-10-17 00:53:21 -0400

    ingreso aplicacion de usuario

--- ObjetoSeguridad/CapaControladorSeguridad/clsControlAsignacionDeAplicaciones.cs
+++ ObjetoSeguridad/CapaControladorSeguridad/clsControlAsignacionDeAplicaciones.cs
@@ -28,6 +28,22 @@ namespace CapaControladorSeguridad
         {
             return asignacionDeAplicaciones.consultaaplicacion();
         }
-
+        public OdbcDataReader consulta_perfiles_asignados(string UserName)
+        {
+            return asignacionDeAplicaciones.consultaperfilasignado(UserName);
+        }
+        //Obtener los datos de la tabla aplicacion.
+        public OdbcDataReader consulta_aplicaciones_asignadas(string UserName)
+        {
+            return asignacionDeAplicaciones.consultaaplicacionasignada(UserName);
+        }
+        public OdbcDataReader consulta_adb(string UserName, string Aplicacion)
+        {
+            return asignacionDeAplicaciones.consultadb(UserName, Aplicacion);
+        }
+        public OdbcDataReader consulta_adbper(string UserName, string Aplicacion)
+        {
+            return asignacionDeAplicaciones.consultadbper(UserName, Aplicacion);
+        }
     }
 }
